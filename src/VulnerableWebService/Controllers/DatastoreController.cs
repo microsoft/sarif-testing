3f16ac8c436830742bcde40e4a107a539a43ec33
Chris Meyer <cmeyer@microsoft.com>
2022-05-16 11:13:05 -1000

    Add vulnerable web service

---
+++ src/VulnerableWebService/Controllers/DatastoreController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Text.RegularExpressions;
+
+namespace VulnerableWebService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DatastoreController : ControllerBase
+    {
+        private readonly ILogger<DatastoreController> logger;
+
+        [HttpGet]
+        public string GetFileContents(string path)
+        {
+            logger.LogTrace($"Getting contents of file '{path}'");
+            return System.IO.File.Exists(path) ? System.IO.File.ReadAllText(path) : null;
+        }
+
+        public string GetDataValue(string token)
+        {
+            string data = "some-data";
+            Match match = Regex.Match(data, "^term=" + token);
+
+            return match.Success ? match.Value : null;
+        }
+    }
+}
